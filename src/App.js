131556400221817c04b10c844b10137fa2acb079
Nicholas Pignatelli <nickpignatelli@icloud.com>
2021-01-28 15:00:38 -0500

    finished middleware w/ axios, and converted the create-todo component to a fucntional one usnig hooks. WOW its so clean

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import CreateTodo from "./components/create-todo.component";
+import CreateTodo from "./components/create-todo.func.component.jsx";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
 
